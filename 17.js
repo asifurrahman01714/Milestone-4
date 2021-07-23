982449c9c265c55207f115fbf5dacb5d1f0237e3
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-23 17:48:15 +0600

    Adding Special Code Snippets

--- 17.js
+++ 17.js
@@ -1,3 +1,10 @@
 /* How to Disable Javascript in a browser 
     Inspect => Settings Options => Debugger Options => Click Disable Javascript
 */
+
+/* ES6 Code Snippets
+  1. Javascript (ES6) Code Snippets
+  2. ESLint
+  3. HTML Code Snippets
+  4. Code Spell Checker
+*/
