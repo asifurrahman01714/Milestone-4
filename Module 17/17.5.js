7aa6908e8d44378ad766a674227c6cff88cff8e9
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-23 18:42:59 +0600

    What is DOM?

---
+++ Module 17/17.5.js
@@ -0,0 +1,7 @@
+/*What is DOM?
+
+    DOM means Document Object Model. When a html file is loaded in browser, 
+    the browser converts the html file in Javascript Object.
+    For this the method is called Document Object Model.
+
+*/
