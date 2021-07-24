1120b5756c89802e5ac0c1fd3770c6f5a5821295
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-24 20:26:51 +0600

    Erasing return

--- Module 18/18.1 Odd and even number.js
+++ Module 18/18.1 Odd and even number.js
@@ -3,22 +3,22 @@ function evenifyAll(array) {
     for (let i = 0; i < array.length; i++) {
         const num = array[i];
         if (num % 2 == 0) {
-            // console.log(num, 'This  number is even')
-            return num;
+            console.log(num, 'This  number is even')
+            // return num;
         } else {
-            // console.log(num*2, 'This number is odd');
-            return num * 2;
+            console.log(num*2, 'This number is odd');
+            // return num * 2;
         }
     }
 }
 
 let nums = [12,34,25,67,34,23];
-// evenifyAll(nums);
-console.log(evenifyAll(nums));
+evenifyAll(nums);
+// console.log(evenifyAll(nums));
 
 let friend_age = [21,22,23,24,25,26,27];
-// evenifyAll(friend_age);
-console.log(evenifyAll(friend_age))
+evenifyAll(friend_age);
+// console.log(evenifyAll(friend_age))
 
 
 // So ..This is the problem. If you want to take multiple result from a 
