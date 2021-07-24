02d2dfb26fe18bd3a115e0f2a792300f69460903
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-24 19:46:11 +0600

    Identify Odd and Even Number From an Array

---
+++ Module 18/18.1 Odd and even number.js
@@ -0,0 +1,10 @@
+let nums = [12,34,25,67,34,23];
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    console.log(element);
+    if (element % 2 == 0) {
+        console.log(element, 'This  number is even')
+    } else {
+        console.log(element, 'This number is odd');
+    }
+}
