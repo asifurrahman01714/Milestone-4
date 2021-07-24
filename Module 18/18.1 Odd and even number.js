afc4c8b201d6ac84182a6d301d837bf68105416a
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-24 20:16:09 +0600

    Deleting All unnessessary code

--- Module 18/18.1 Odd and even number.js
+++ Module 18/18.1 Odd and even number.js
@@ -1,12 +1,6 @@
 let nums = [12,34,25,67,34,23];
 for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
-   
-    // if (num % 2 == 0) {
-    //     console.log(num, 'This  number is even')
-    // } else {
-    //     console.log(num*2, 'This number is odd');
-    // }
     evenify(num);
 }
 
@@ -21,12 +15,5 @@ function evenify(num) {
 let friend_age = [21,22,23,24,25,26,27];
 for (let i = 0; i < friend_age.length; i++) {
     const age = friend_age[i];
-    
-    // if (num % 2 == 0) {
-    //     console.log(num, 'This  number is even')
-    // } else {
-    //     console.log(num*2, 'This number is odd');
-    // }
-
     evenify(age);
 }
