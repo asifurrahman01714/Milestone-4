7b3b9d0cd36275879f989f619fc76cb271cabb24
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-24 20:15:18 +0600

    Fetch All duplicate function in a common function

--- Module 18/18.1 Odd and even number.js
+++ Module 18/18.1 Odd and even number.js
@@ -1,10 +1,32 @@
 let nums = [12,34,25,67,34,23];
 for (let i = 0; i < nums.length; i++) {
-    const element = nums[i];
-    console.log(element);
-    if (element % 2 == 0) {
-        console.log(element, 'This  number is even')
+    const num = nums[i];
+   
+    // if (num % 2 == 0) {
+    //     console.log(num, 'This  number is even')
+    // } else {
+    //     console.log(num*2, 'This number is odd');
+    // }
+    evenify(num);
+}
+
+function evenify(num) {
+    if (num % 2 == 0) {
+        console.log(num, 'This  number is even')
     } else {
-        console.log(element*2, 'This number is odd');
+        console.log(num*2, 'This number is odd');
     }
+}
+
+let friend_age = [21,22,23,24,25,26,27];
+for (let i = 0; i < friend_age.length; i++) {
+    const age = friend_age[i];
+    
+    // if (num % 2 == 0) {
+    //     console.log(num, 'This  number is even')
+    // } else {
+    //     console.log(num*2, 'This number is odd');
+    // }
+
+    evenify(age);
 }
