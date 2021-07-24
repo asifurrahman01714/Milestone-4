71fbf4afd9550ea0d6d9ce21da6408f7e19a9e8b
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-24 20:26:06 +0600

    Don't use return in a function if you need to access multiple result

--- Module 18/18.1 Odd and even number.js
+++ Module 18/18.1 Odd and even number.js
@@ -1,16 +1,25 @@
+// Assigning All duplicate code in a single function;
 function evenifyAll(array) {
     for (let i = 0; i < array.length; i++) {
         const num = array[i];
         if (num % 2 == 0) {
-            console.log(num, 'This  number is even')
+            // console.log(num, 'This  number is even')
+            return num;
         } else {
-            console.log(num*2, 'This number is odd');
+            // console.log(num*2, 'This number is odd');
+            return num * 2;
         }
     }
 }
 
 let nums = [12,34,25,67,34,23];
-evenifyAll(nums);
+// evenifyAll(nums);
+console.log(evenifyAll(nums));
 
 let friend_age = [21,22,23,24,25,26,27];
-evenifyAll(friend_age);
+// evenifyAll(friend_age);
+console.log(evenifyAll(friend_age))
+
+
+// So ..This is the problem. If you want to take multiple result from a 
+// function don't use return. Because return will give you only a single result.So use console In this case
