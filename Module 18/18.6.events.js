e2a4acc5fb360c70153994e7ec1a3caa15dc9ed2
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 22:16:59 +0600

    Onsubmit Event

--- Module 18/18.6.events.js
+++ Module 18/18.6.events.js
@@ -30,3 +30,29 @@ An example of HTML onclick() event:
 </html>
 
 */
+
+/**
+ On submit Events
+
+ <html>
+   <head>   
+      <script type = "text/javascript">
+         <!--
+            function validation() {
+               all validation goes here
+               .........
+               return either true or false
+            }
+         //-->
+      </script>      
+   </head>
+   
+   <body>   
+      <form method = "POST" action = "t.cgi" onsubmit = "return validate()">
+         .......
+         <input type = "submit" value = "Submit" />
+      </form>      
+   </body>
+</html>
+
+ */
