58ccbcad991d9a39026af05ede3aaab1949ef9fb
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 22:15:46 +0600

    Onclick Event

--- Module 18/18.6.events.js
+++ Module 18/18.6.events.js
@@ -7,3 +7,26 @@ Here are some examples of HTML events:
 2. An HTML input field was changed
 3. An HTML button was clicked
 */
+
+/* 
+An example of HTML onclick() event:
+<html>
+   <head>   
+      <script type = "text/javascript">
+         <!--
+            function sayHello() {
+               alert("Hello World")
+            }
+         //-->
+      </script>      
+   </head>
+   
+   <body>
+      <p>Click the following button and see result</p>      
+      <form>
+         <input type = "button" onclick = "sayHello()" value = "Say Hello" />
+      </form>      
+   </body>
+</html>
+
+*/
