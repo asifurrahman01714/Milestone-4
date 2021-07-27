23ac7ea5873a4030304c78a81c23d9ace5011f12
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:25:34 +0600

    drive for loop on arguments

--- Module 18/18.4.Arguments.js
+++ Module 18/18.4.Arguments.js
@@ -2,7 +2,12 @@
 
 function addNumbers(num1, num2) {
     console.log(arguments);// arguemnts is a object like array.
-    return num1 + num2;
+    let sum=0;
+    for (let i = 0; i < arguments.length; i++) {
+        const element = arguments[i];
+        sum = sum + element;
+    }
+    return sum;
 }
 let result = addNumbers(2,3,4,5);
 console.log(result);
