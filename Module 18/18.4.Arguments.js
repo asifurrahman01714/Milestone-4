9f4b711d1d06e6ca66a74a92353bb3a7b6f3fa00
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:36:50 +0600

    Don't call a variable with var before declaring it.

--- Module 18/18.4.Arguments.js
+++ Module 18/18.4.Arguments.js
@@ -3,6 +3,7 @@
 function addNumbers(num1, num2) {
     console.log(arguments);// arguemnts is a object like array.
     let sum=0;
+    console.log(double) // Don't call a variable with var before declaring it.It will not occured any error just show you undefined.'
     for (let i = 0; i < arguments.length; i++) {
         const element = arguments[i];
         sum = sum + element;
@@ -11,6 +12,7 @@ function addNumbers(num1, num2) {
         console.log(message);
     }
     logInfo('Hei,Call a function after declaring it. Although any error not created but it is a good practice/')
+    var double = sum * 2;
     return sum;
 }
 let result = addNumbers(2,3,4,5);
