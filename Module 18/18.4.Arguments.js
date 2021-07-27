6f93c46c21b37252bf91bf30853d867b475e830e
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:22:52 +0600

    arguemnts is a object like array

--- Module 18/18.4.Arguments.js
+++ Module 18/18.4.Arguments.js
@@ -1,7 +1,7 @@
 // Arguments in Javascript:
 
 function addNumbers(num1, num2) {
-    console.log(arguments);
+    console.log(arguments);// arguemnts is a object like array.
     return num1 + num2;
 }
 let result = addNumbers(2,3,4,5);
