5260cb9c25be49c64e6c17e6f4b35fc581f3c0f6
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:13:07 +0600

    callback Done

--- Module 18/18.3.Callback Function.js
+++ Module 18/18.3.Callback Function.js
@@ -1,2 +1,15 @@
 // Callback Function: The function which is called by another function parameter.
 
+function mainFunction(name, age, task) {
+    console.log('Your Name is', name);
+    console.log('Your age is', age);
+    task();
+}
+function washHand() {
+    console.log("wash Hand")
+}
+function takeShower() {
+    console.log('Take Shower');
+}
+mainFunction('Asif', 24, washHand);
+mainFunction('Sakib', 21, takeShower);
