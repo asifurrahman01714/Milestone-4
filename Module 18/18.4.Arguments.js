3099872c9eeab3495b2e4defba455a0d0f4fefdc
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:22:06 +0600

    Console log arguemnts

--- Module 18/18.4.Arguments.js
+++ Module 18/18.4.Arguments.js
@@ -1,7 +1,8 @@
 // Arguments in Javascript:
 
 function addNumbers(num1, num2) {
+    console.log(arguments);
     return num1 + num2;
 }
-let result = addNumbers(2,3);
+let result = addNumbers(2,3,4,5);
 console.log(result);
