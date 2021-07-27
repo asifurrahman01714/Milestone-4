abfe9b173545269b290ed3c33b91b6bbbcac624c
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-27 18:38:39 +0600

    Don't try access any variable with const before declaring it

--- Module 18/18.4.Arguments.js
+++ Module 18/18.4.Arguments.js
@@ -13,6 +13,8 @@ function addNumbers(num1, num2) {
     }
     logInfo('Hei,Call a function after declaring it. Although any error not created but it is a good practice/')
     var double = sum * 2;
+    console.log(tripple); // If you try access any variable with const before declaring it. It will give you error. So don't do this.
+    const tripple = sum * 3;
     return sum;
 }
 let result = addNumbers(2,3,4,5);
