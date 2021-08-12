d4fb4a1d00f08b9ba0ab75f160110a0c8a401459
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-12 22:12:47 +0600

    capturing phoneInput and convert it into integer number

--- Module 19.5.shopping-cart/script.js
+++ Module 19.5.shopping-cart/script.js
@@ -1,5 +1,8 @@
 const phonePlus = document.getElementById('phonePlus');
 phonePlus.addEventListener('click',function(e){
     console.log('phone plus clicked');
-
+    const phoneInput = document.getElementById('phoneInput');
+    console.log(phoneInput.value);
+    const phoneInputNumber = parseInt(phoneInput.value);
+    console.log(phoneInputNumber);
 })
