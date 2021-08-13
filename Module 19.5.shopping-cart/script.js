955f3e6468deeb0bae11d8f75872688076a6f04d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-13 12:08:35 +0600

    handleProductChange function created to work multiple work

--- Module 19.5.shopping-cart/script.js
+++ Module 19.5.shopping-cart/script.js
@@ -1,39 +1,31 @@
 const phonePlus = document.getElementById('phonePlus');
 phonePlus.addEventListener('click',function(e){
-    console.log('phone plus clicked');
-
-    const phoneInput = document.getElementById('phoneInput');
-    console.log(phoneInput.value);
-    const phoneInputNumber = parseInt(phoneInput.value);
-    console.log(phoneInputNumber);
-    const phoneNewInputNumber = phoneInputNumber + 1;
-    console.log("phoneNewInputNumber", phoneNewInputNumber);
-    phoneInput.value = phoneNewInputNumber;
-
-
-    const phonePrice = document.getElementById('phonePrice');
-   
-    phonePrice.innerText = 100 * phoneNewInputNumber;
+    handleProductChange(true);
 })
 
 const phoneMinus = document.getElementById('phoneMinus');
 phoneMinus.addEventListener('click',function(e){
+    handleProductChange(false);
+});
+
+function handleProductChange(isIncrease) {
     console.log('phone minus clicked');
-    
-    
+
     const phoneInput = document.getElementById('phoneInput');
     console.log(phoneInput.value);
-    const phoneInputNumber = parseInt(phoneInput.value);
+    let phoneInputNumber = parseInt(phoneInput.value);
     console.log(phoneInputNumber);
-    const phoneNewInputNumber = phoneInputNumber - 1;
+    let phoneNewInputNumber;
+    if (isIncrease) {
+        phoneNewInputNumber = phoneInputNumber + 1;
+    } else {
+        phoneNewInputNumber = phoneInputNumber - 1;
+    }
     console.log("phoneNewInputNumber", phoneNewInputNumber);
 
     if (phoneNewInputNumber >= 0) {
         phoneInput.value = phoneNewInputNumber;
         const phonePrice = document.getElementById('phonePrice');
         phonePrice.innerText = 100 * phoneNewInputNumber;
-    } else {
-        return "Negative product is not acceptable";
     }
-
-})
+}
