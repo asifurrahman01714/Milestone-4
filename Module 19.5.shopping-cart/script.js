10d9286fe46a5a03f5b76ca42039d38c30034eba
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-13 14:43:21 +0600

    trying to work all things within 1 function only

--- Module 19.5.shopping-cart/script.js
+++ Module 19.5.shopping-cart/script.js
@@ -1,19 +1,13 @@
-
 function phoneProductChange(isIncrease) {
-    console.log('phone minus clicked');
-
+    
     const phoneInput = document.getElementById('phoneInput');
-    console.log(phoneInput.value);
     let phoneInputNumber = parseInt(phoneInput.value);
-    console.log(phoneInputNumber);
     let phoneNewInputNumber;
     if (isIncrease) {
         phoneNewInputNumber = phoneInputNumber + 1;
     } else {
         phoneNewInputNumber = phoneInputNumber - 1;
     }
-    console.log("phoneNewInputNumber", phoneNewInputNumber);
-
     if (phoneNewInputNumber >= 0) {
         phoneInput.value = phoneNewInputNumber;
         const phonePrice = document.getElementById('phonePrice');
@@ -22,24 +16,49 @@ function phoneProductChange(isIncrease) {
 }
 
 
-function caseProductChange(isIncrease) {
-    console.log('case minus clicked');
 
-    const caseInput = document.getElementById('caseInput');
-    console.log(caseInput.value);
-    let caseInputNumber = parseInt(caseInput.value);
-    console.log(caseInputNumber);
-    let caseNewInputNumber;
-    if (isIncrease) {
-        caseNewInputNumber = caseInputNumber + 1;
-    } else {
-        caseNewInputNumber = caseInputNumber - 1;
-    }
-    console.log("phoneNewInputNumber", caseNewInputNumber);
 
-    if (caseNewInputNumber >= 0) {
-        caseInput.value = caseNewInputNumber;
-        const casePrice = document.getElementById('casePrice');
-        casePrice.innerText = 50 * caseNewInputNumber;
-    }
-}
+// function phoneProductChange(isIncrease) {
+//     console.log('phone minus clicked');
+
+//     const phoneInput = document.getElementById('phoneInput');
+//     console.log(phoneInput.value);
+//     let phoneInputNumber = parseInt(phoneInput.value);
+//     console.log(phoneInputNumber);
+//     let phoneNewInputNumber;
+//     if (isIncrease) {
+//         phoneNewInputNumber = phoneInputNumber + 1;
+//     } else {
+//         phoneNewInputNumber = phoneInputNumber - 1;
+//     }
+//     console.log("phoneNewInputNumber", phoneNewInputNumber);
+
+//     if (phoneNewInputNumber >= 0) {
+//         phoneInput.value = phoneNewInputNumber;
+//         const phonePrice = document.getElementById('phonePrice');
+//         phonePrice.innerText = 100 * phoneNewInputNumber;
+//     }
+// }
+
+
+// function caseProductChange(isIncrease) {
+//     console.log('case minus clicked');
+
+//     const caseInput = document.getElementById('caseInput');
+//     console.log(caseInput.value);
+//     let caseInputNumber = parseInt(caseInput.value);
+//     console.log(caseInputNumber);
+//     let caseNewInputNumber;
+//     if (isIncrease) {
+//         caseNewInputNumber = caseInputNumber + 1;
+//     } else {
+//         caseNewInputNumber = caseInputNumber - 1;
+//     }
+//     console.log("phoneNewInputNumber", caseNewInputNumber);
+
+//     if (caseNewInputNumber >= 0) {
+//         caseInput.value = caseNewInputNumber;
+//         const casePrice = document.getElementById('casePrice');
+//         casePrice.innerText = 50 * caseNewInputNumber;
+//     }
+// }
