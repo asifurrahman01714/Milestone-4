4e90eca83a80938e286ec88e0481a78eff3c7d46
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-19 23:33:02 +0600

    Adding firstClassMinus btn work

--- Module 21 Assigment 4/script.js
+++ Module 21 Assigment 4/script.js
@@ -5,4 +5,18 @@ firstClassPlusBtn.addEventListener('click',function(e){
     console.log("Before Increasing Value",firstClassInput);
     firstClass.value = firstClassInput + 1;
     console.log("After Increasing Value",firstClass.value);
+});
+
+const firstClassMinusBtn = document.getElementById('firstClassMinusBtn');
+firstClassMinusBtn.addEventListener('click',function(e){
+    const firstClass = document.getElementById('firstClass');
+    const firstClassInput = parseInt(firstClass.value);
+    console.log("Before Increasing Value",firstClassInput);
+    if (firstClassInput > 0) {
+        firstClass.value = firstClassInput - 1;
+    }
+    else{
+        return "It is not possible negative Input";
+    }
+    console.log("After Increasing Value",firstClass.value);
 })
