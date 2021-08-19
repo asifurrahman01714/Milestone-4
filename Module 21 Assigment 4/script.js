ba50c6e40ca144a767f8525155fae132fb6a8803
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-19 23:30:48 +0600

    Increasing Phone Input

--- Module 21 Assigment 4/script.js
+++ Module 21 Assigment 4/script.js
@@ -2,5 +2,7 @@ const firstClassPlusBtn = document.getElementById('firstClassPlusBtn');
 firstClassPlusBtn.addEventListener('click',function(e){
     const firstClass = document.getElementById('firstClass');
     const firstClassInput = parseInt(firstClass.value);
-    console.log(firstClassInput);
+    console.log("Before Increasing Value",firstClassInput);
+    firstClass.value = firstClassInput + 1;
+    console.log("After Increasing Value",firstClass.value);
 })
