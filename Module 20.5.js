1002b774393822c8eed7bf9eb7f06a36878f6298
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-19 22:35:23 +0600

    passing a function as  another function parameter

--- Module 20.5.js
+++ Module 20.5.js
@@ -1,5 +1,9 @@
-function welcomeGuest(name) {
-    console.log(name);
+function welcomeGuest(name, greetHandler) {
+    greetHandler(name); // here greetHandler is like greetMorning That means by this function actually greetMorning is called
 }
 const actorName = 'Asif';
-welcomeGuest(actorName);
+function greetMorning(name) {
+    console.log('Good Morning ' + name);
+}
+welcomeGuest(actorName, greetMorning); // Passing greetMorning through welcomeGuest function
+
