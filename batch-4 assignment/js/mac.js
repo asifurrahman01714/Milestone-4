9bb9b0298ab356e958bac5f9e7f48580e4830caf
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-21 13:59:19 +0600

    completed

--- batch-4 assignment/js/mac.js
+++ batch-4 assignment/js/mac.js
@@ -1,10 +1,10 @@
-const memory8GB = document.getElementById('memory8GB');
-const memory16GB = document.getElementById('memory16GB');
+const memoryBtn8GB = document.getElementById('memoryBtn8GB');
+const memoryBtn16GB = document.getElementById('memoryBtn16GB');
 
 //storeag cost
-const storage256GB = document.getElementById('storage256GB');
-const storage512GB = document.getElementById('storage512GB');
-const storage1T = document.getElementById('storage1T');
+const storageBtn256GB = document.getElementById('storageBtn256GB');
+const storageBtn512GB = document.getElementById('storageBtn512GB');
+const storageBtn1T = document.getElementById('storageBtn1T');
 
 //shipping
 const free = document.getElementById('free');
@@ -14,28 +14,38 @@ const memoryField = document.getElementById('shippincost');
 const storageField = document.getElementById('storageField');
 const shippingField = document.getElementById('shippingField');
 const subTotalField = document.getElementById('subTotal');
+
+
+
+// Total Price
+let totalPrice = document.getElementById('totalPrice');
+let grandTotal = document.getElementById('grandTotal');
+
 //memory
-memory8GB.addEventListener('click', function () {
-    memoryField.innerText = '0';
+memoryBtn8GB.addEventListener('click', function () {
+    memoryField.innerText = 0;
 
 });
-memory16GB.addEventListener('click', function () {
-    memoryField.innerText = '180';
-
+memoryBtn16GB.addEventListener('click', function () {
+    memoryField.innerText = 180;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( memoryField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
 });
 
 //storage 
-storage256GB.addEventListener('click', function () {
-    storageField.innerText = '0';
+storageBtn256GB.addEventListener('click', function () {
+    storageField.innerText = 0;
 
 });
-storage512GB.addEventListener('click', function () {
-    storageField.innerText = '100';
-
+storageBtn512GB.addEventListener('click', function () {
+    storageField.innerText =  100 ;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( storageField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
 });
-storage1T.addEventListener('click', function () {
-    storageField.innerText = '180';
-
+storageBtn1T.addEventListener('click', function () {
+    storageField.innerText = 180;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( storageField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
 
 });
 
@@ -44,18 +54,22 @@ free.addEventListener('click', function () {
     shippingField.innerText = '0';
 });
 paid.addEventListener('click', function () {
-    shippingField.innerText = '20';
+    shippingField.innerText = 20;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( shippingField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
 });
 
-//update total
-function getMS() {
-    const memoryTotla = Number(memoryField.innerText);
-    const storeTotla = Number(storageField.innerText);
-
-    const granTotal = memoryTotla + storeTotla;
-    subTotalField.innerText = granTotal;
 
 
-    //const memoryTotla = Number(memoryField.innerText);
+// //update total
+// function getMS() {
+//     const memoryTotla = Number(memoryField.innerText);
+//     const storeTotla = Number(storageField.innerText);
 
-}
+//     const granTotal = memoryTotla + storeTotla;
+//     subTotalField.innerText = granTotal;
+
+
+//     //const memoryTotla = Number(memoryField.innerText);
+
+// }
