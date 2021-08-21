5045e2dc7fc2e2b56a9d9d0afaa9e7af87932a23
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-21 12:25:34 +0600

    Megrafriend added

---
+++ Js-String-Array-Phero/array.exists.js
@@ -0,0 +1,20 @@
+function megaFriend(friends) {
+    if (Array.isArray(friends) == false) {
+        return 'Please provide an array';
+    }
+    console.log(Array.isArray(friends));
+    let mega = friends[0];
+    for (const friend of friends) {
+        if (friend.length > mega.length) {
+            mega = friend;
+        }
+    }
+    return mega;
+}
+const friends = ['kutub', 'Lion', 'Shamol', 'Sabbir'];
+const myBigBuddy = megaFriend(friends);
+console.log(myBigBuddy);
+
+const nonArray = megaFriend(1245);
+console.log(nonArray)
+
