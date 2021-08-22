c15391044ab9a4388cbab1a161150b53d3b6d455
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-08-22 22:20:22 +0600

    Previous code saved

--- batch-4 assignment/js/mac.js
+++ batch-4 assignment/js/mac.js
@@ -1,96 +1,75 @@
+const memoryBtn8GB = document.getElementById('memoryBtn8GB');
+const memoryBtn16GB = document.getElementById('memoryBtn16GB');
 
-//memory section
-const memory = document.getElementById('memory16');
-memory.addEventListener('click', function () {
-    document.getElementById('memory-cost').innerText = '180';
-    netPrice();
+//storeag cost
+const storageBtn256GB = document.getElementById('storageBtn256GB');
+const storageBtn512GB = document.getElementById('storageBtn512GB');
+const storageBtn1T = document.getElementById('storageBtn1T');
+
+//shipping
+const free = document.getElementById('free');
+const paid = document.getElementById('paid');
+
+const memoryField = document.getElementById('shippincost');
+const storageField = document.getElementById('storageField');
+const shippingField = document.getElementById('shippingField');
+const subTotalField = document.getElementById('subTotal');
+
+
+
+// Total Price
+let totalPrice = document.getElementById('totalPrice');
+let grandTotal = document.getElementById('grandTotal');
+
+//memory
+memoryBtn8GB.addEventListener('click', function () {
+    memoryField.innerText = 0;
+
+});
+memoryBtn16GB.addEventListener('click', function () {
+    memoryField.innerText = 180;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + 180;
+    grandTotal.innerText = totalPrice.innerText;
 });
 
-const memory2 = document.getElementById('memory8');
-memory2.addEventListener('click', function () {
-    document.getElementById('memory-cost').innerText = '0';
-    netPrice();
+//storage 
+storageBtn256GB.addEventListener('click', function () {
+    storageField.innerText = 0;
+
+});
+storageBtn512GB.addEventListener('click', function () {
+    storageField.innerText =  100 ;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( storageField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
+});
+storageBtn1T.addEventListener('click', function () {
+    storageField.innerText = 180;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( storageField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
+
 });
 
-//Memory storage
-//256gb button
-const storage = document.getElementById('storage256');
-storage.addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = '0';
-    netPrice();
+//shipping
+free.addEventListener('click', function () {
+    shippingField.innerText = '0';
+});
+paid.addEventListener('click', function () {
+    shippingField.innerText = 20;
+    totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt( shippingField.innerText);
+    grandTotal.innerText = totalPrice.innerText;
 });
 
-//512gb button
-const storagee = document.getElementById('storage512');
-storagee.addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = '100';
-    netPrice();
-});
-
-//1tb button
-const lastStorage = document.getElementById('storage1tb');
-lastStorage.addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = '180';
-    netPrice();
-});
-
-//delivery section
-const freeDelivery = document.getElementById('free-delivery');
-freeDelivery.addEventListener('click', function () {
-    document.getElementById('delivery-const').innerText = '0';
-    netPrice();
-});
-
-const delivery = document.getElementById('delivery');
-delivery.addEventListener('click', function () {
-    document.getElementById('delivery-const').innerText = '20';
-    netPrice();
-});
-
-// sum of all the prices
-function netPrice() {
-    const bestPriceCost = parseInt(
-        document.getElementById('best-price').innerText
-    );
-
-    const deliveryCost = parseInt(
-        document.getElementById('delivery-const').innerText
-    );
-
-    const memoryCost = parseInt(document.getElementById('memory-cost').innerText);
-
-    const storageCost = parseInt(
-        document.getElementById('storage-cost').innerText
-    );
-
-    let netCostPrice = bestPriceCost + deliveryCost + memoryCost + storageCost;
-
-    const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = netCostPrice;
-
-    const lastTotal = document.getElementById('lastTotal');
-    lastTotal.innerText = netCostPrice;
 
 
-}
+// //update total
+// function getMS() {
+//     const memoryTotla = Number(memoryField.innerText);
+//     const storeTotla = Number(storageField.innerText);
 
-document.getElementById('promo-btn').addEventListener('click', function () {
-    promoCode()
-});
-
-function promoCode() {
-    const totalPrice = document.getElementById('total-price').innerText;
-    const promoCodeInput = document.getElementById('promo-Input');
-    const promoCode = promoCodeInput.value
-    const givenPromoCode = 'stevejobs';
-    if (promoCode == givenPromoCode) {
-        const discountPrice = parseFloat(totalPrice) * 20 / 100;
-        const totalDiscount = parseFloat(totalPrice) - discountPrice;
-        const updateTotal = document.getElementById('lastTotal');
-        updateTotal.innerText = totalDiscount;
-        promoCodeInput.value = '';
-    }
-};
+//     const granTotal = memoryTotla + storeTotla;
+//     subTotalField.innerText = granTotal;
 
 
+//     //const memoryTotla = Number(memoryField.innerText);
 
+// }
